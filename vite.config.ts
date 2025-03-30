4bb153485c91ded414ea6be862e3635c5981c231
Arif GEVENCI <gevenci.arif@gmail.com>
2025-03-30 21:26:21 +0300

    vite config bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,7 @@ import react from "@vitejs/plugin-react";
 import autoprefixer from "autoprefixer";
 import path from "path";
 import tailwindcss from "tailwindcss";
-import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
