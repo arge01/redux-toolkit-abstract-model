3aaf0b5df1b6e8c92a3dd49fe35ed95dbee46595
Arif GEVENCI <gevenci.arif@gmail.com>
2025-03-30 21:28:30 +0300

    bug fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     alias: [
       {
         find: "@",
-        replacement: path.resolve(__dirname, "./src"),
+        replacement: path.resolve("./src"),
       },
     ],
   },
